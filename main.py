56b989d7e3dfc7182bb305b0187bb378f39bc6a1
sahbaz23 <amdsahbaz19@gmail.com>
2026-05-18 11:03:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# from fastapi import FastAPI
+
+# app= FastAPI()
+
+
+# #Home Route
+# @app.get("/")
+# def home():
+#     return {"message": "Hello World"}
+
+
+# @app.get("/about")
+# def about():
+#     return {"message": "This is the about page"}
+
+# @app.get("/contact")
+# def contact():
+#     return {"message": "This is the contact page"}
