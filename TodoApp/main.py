d196fc866b113e466af6e729c2668da3c1a2ce37
sahbaz23 <amdsahbaz19@gmail.com>
2026-05-18 13:26:43 +0530

    add status code and HTTP exception in Todo App

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status,HTTPException
 from pydantic import BaseModel
 
 app=FastAPI()
@@ -13,29 +13,30 @@ class Todo(BaseModel):
     completed:bool=False
     
     
-@app.post("/todos")
+@app.post("/todos",status_code=status.HTTP_201_CREATED)
 
 def create_todo(todo:Todo):
     todos.append(todo)
     return {"message":"Todo created successfully","data":todo}
 
 
-@app.get("/todos")
+@app.get("/todos", status_code=status.HTTP_200_OK)
 
 def get_todos():
+    
     return {"message":"List of todos","data":todos}
 
 
-@app.get("/todos/{todo_id}")
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 
 def get_todo(todo_id:int):
     for todo in todos:
         if todo.id==todo_id:
             return {"message":"Todo found","data":todo}
-    return {"message":"Todo not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
 
 
-@app.put("/todos/{todo_id}")
+@app.put("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 
 
 def update_todo(todo_id:int,updated_todo:Todo):
@@ -43,16 +44,16 @@ def update_todo(todo_id:int,updated_todo:Todo):
         if todo.id==todo_id:
             todos[index]=updated_todo
             return {"message":"Todo updated successfully","data":updated_todo}
-    return {"message":"Todo not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
 
 
-@app.delete("/todos/{todo_id}")
+@app.delete("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 
 def delete_todo(todo_id:int):
     for index,todo in enumerate(todos):
         if todo.id==todo_id:
             deleted_todo=todos.pop(index)
             return {"message":"Todo deleted successfully","data":deleted_todo}
-    return {"message":"Todo not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
 
 
