b5bf13064bd29b31d3aa10694d64bbef2ca68985
sahbaz23 <amdsahbaz19@gmail.com>
2026-05-21 14:16:12 +0530

    Third party APi use

--- cors.py
+++ cors.py
@@ -42,3 +42,16 @@ app= FastAPI()
 #         "result": a - b
 #     }
 
+
+import requests
+
+@app.get("/posts")
+
+def get_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    return response.json()
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    response = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
+    return response.json()
